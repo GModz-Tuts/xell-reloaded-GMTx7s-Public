706fcc46a95f9bc8a24cdb6a73c31b18ae792771
Hostility <optic-phazedd@live.com>
2025-03-25 23:11:31 -0600

    Update main.c
    Black on Red

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -116,7 +116,7 @@ int main(){
 #elif defined XTUDO_THEME
     console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK); // Pink text on black bg
 #elif defined DEFAULT_THEME
-    console_set_colors(CONSOLE_COLOR_RED,CONSOLE_COLOR_BACK); // Black text on red bg
+    console_set_colors(CONSOLE_COLOR_RED,CONSOLE_COLOR_BLACK); // Black text on red bg
 #else
     console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN); // Green text on black bg
 #endif
