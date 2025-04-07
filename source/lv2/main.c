1f983013080d295971b1ea4b3fb2645527fd9795
Hostility <optic-phazedd@live.com>
2025-04-06 22:28:52 -0600

    Update main.c
    black on orange

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -114,7 +114,7 @@ int main(){
 #ifdef SWIZZY_THEME
     console_set_colors(CONSOLE_COLOR_RED,CONSOLE_COLOR_WHITE); // White text on red bg
 #elif defined XTUDO_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_ORANGE); // Orange text on black bg
+    console_set_colors(CONSOLE_COLOR_ORANGE,CONSOLE_COLOR_BLACK); // Black text on orange bg
 #elif defined DEFAULT_THEME
     console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_RED); // Red text on black bg
 #else
